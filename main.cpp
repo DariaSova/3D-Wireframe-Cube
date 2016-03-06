10ce205915116168ae16f8f58cb35383e12e7371
Daria Sova <todariasova@gmail.com>
2016-03-06 15:45:59 -0800

    WIP: make verything float

--- main.cpp
+++ main.cpp
@@ -8,18 +8,18 @@ unsigned int height = 512;
 
 Canvas canvas;
 float distance = 10.0f;
-Vector3d camera(1, 0, 0);
+Vector3f camera(1, 0, 0);
 float n = 1.0f;
 float f = 5.0f;
 float r = 1.0f;
 float t = 1.0f;
 float b = -1.0f;
 float l = -1.0f;
-std::vector<Vector4d> points;
-Matrix4d viewport_matrix;
-Matrix4d perspective_matrix;
-Matrix4d camera_matrix;
-Matrix4d final_matrix;
+std::vector<Vector4f> points;
+Matrix4f viewport_matrix;
+Matrix4f perspective_matrix;
+Matrix4f camera_matrix;
+Matrix4f final_matrix;
 
 
 void InitializeGL()
@@ -44,15 +44,16 @@ void KeyPress(char keychar)
 
 void OnPaint()
 {
-    printf("Matrix Size: %lu\n", points.size());
+   // printf("Matrix Size: %lu\n", points.size());
 
     for(int i=0; i<points.size(); i++)///i+2???
     {
-        Vector4d p = final_matrix*points[i];
-        Vector4d q = final_matrix*points[i+1];
-        printf("Point X: %f, %f Y: %f, %f\n", p[0], p[2], q[0], q[2]);
+        Vector4f p = final_matrix*points[i];
+        Vector4f q = final_matrix*points[i+1];
+        printf("Point X: %f, %f Y: %f, %f\n", p[0], p[1], q[0], q[1]);
 
         canvas.AddLine(p[0]/p[2], p[1]/p[2], q[0]/q[2], q[1]/q[2]);
+        //canvas.AddLine(0,0.2,0.9,0.9);
 
     }
 }
@@ -63,14 +64,14 @@ void OnTimer()
 }
 
 int main(int, char **){
-    points.push_back(Vector4d(1, 1, 1, 1));
-    points.push_back(Vector4d(1, -1, 1, 1));
-    points.push_back(Vector4d(1, -1, -1, 1));
-    points.push_back(Vector4d(1, 1, -1, 1));
-    points.push_back(Vector4d(-1, 1, 1, 1));
-    points.push_back(Vector4d(-1, -1, 1, 1));
-    points.push_back(Vector4d(-1, -1, -1, 1));
-    points.push_back(Vector4d(-1, 1, -1, 1));
+    points.push_back(Vector4f(1, 1, 1, 1));
+    points.push_back(Vector4f(1, -1, 1, 1));
+    points.push_back(Vector4f(1, -1, -1, 1));
+    points.push_back(Vector4f(1, 1, -1, 1));
+    points.push_back(Vector4f(-1, 1, 1, 1));
+    points.push_back(Vector4f(-1, -1, 1, 1));
+    points.push_back(Vector4f(-1, -1, -1, 1));
+    points.push_back(Vector4f(-1, 1, -1, 1));
 
     //compute Mvp
     viewport_matrix <<
@@ -82,35 +83,39 @@ int main(int, char **){
     //compute Mperspective=Morth*Mper
     perspective_matrix <<
             2*n/(r-l), 0, (l+r)/(l-r), 0,
-            0, 2*n*(t-b), (b+t)/(b-t), 0,
-            0, 0, (n+f)/(n-f), -2*f*n/(f-n),
+            0, 2*n/(t-b), (b+t)/(b-t), 0,
+            0, 0, (n+f)/(n-f), 2*f*n/(f-n),
             0, 0, 1, 0;
 
     //compute Mcamera
-    Vector3d eye_position = camera * distance;
-    Vector3d gaze_dir= eye_position*(-1);
-    Vector3d up_vector(0, 1, 0);
-    Vector3d w = -(gaze_dir/gaze_dir.norm());
-    Vector3d u = ((up_vector.cross(w))/(up_vector.cross(w)).norm());
-    Vector3d v = w.cross(u);
+    Vector3f eye_position = camera* distance;
+    Vector3f gaze_dir= eye_position*(-1);
+    Vector3f up_vector(0, 1, 0);
+    Vector3f w = -(gaze_dir/gaze_dir.norm());
+    Vector3f u = ((up_vector.cross(w))/(up_vector.cross(w)).norm());
+    Vector3f v = w.cross(u);
 
-    Matrix4d MV;
+    Matrix4f MV;
             MV <<
                u[0], u[1], u[2], 0,
                v[0], v[1], v[2], 0,
                w[0], w[1], w[2], 0,
                0, 0, 0, 1;
+    printf("MV X: %f, %f Y: %f, %f\n", MV(0,0), MV(0,1), MV(0,2), MV(0,3));
 
-    Matrix4d temp;
+    Matrix4f temp;
     temp <<
           1, 0, 0, -eye_position[0],
           0, 1, 0, -eye_position[1],
           0, 0, 1, -eye_position[2],
           0, 0, 0, 1;
+    printf("TEMP X: %f, %f Y: %f, %f\n", temp(0,0), temp(0,1), temp(0,2), temp(0,3));
 
     camera_matrix = MV*temp;
 
-    final_matrix= viewport_matrix*perspective_matrix*camera_matrix;
+   //final_matrix= viewport_matrix*perspective_matrix*camera_matrix;
+    final_matrix= perspective_matrix*camera_matrix;
+    printf("FINAL X: %f, %f Y: %f, %f\n", final_matrix(0,0), final_matrix(0,1), final_matrix(0,2), final_matrix(0,3));
     //draw lines for all the points
 
     //Link the call backs
