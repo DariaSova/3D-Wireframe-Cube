95d4a573d7930b0b8a9e73eff425f395fadfa663
Daria Sova <todariasova@gmail.com>
2016-03-05 20:41:55 -0800

    Add perspective matrix

--- main.cpp
+++ main.cpp
@@ -7,8 +7,16 @@ unsigned int width = 512;
 unsigned int height = 512;
 
 Canvas canvas;
-
+float Distance = 5.0f;
+float n = 1.0f;
+float f = 5.0f;
+float r = 1.0f;
+float t = 1.0f;
+float b = -1.0f;
+float l = -1.0f;
 std::vector<Vector4d> points;
+Matrix4d perspective_matrix;
+
 
 void InitializeGL()
 {
@@ -49,6 +57,17 @@ int main(int, char **){
     points.push_back(Vector4d(-1, -1, 1, 1));
     points.push_back(Vector4d(-1, -1, -1, 1));
     points.push_back(Vector4d(-1, 1, -1, 1));
+
+    //compute Mvp
+    //compute Mperspective=Morth*Mper
+    perspective_matrix <<
+            2*n/(r-l), 0, (l+r)/(l-r), 0,
+            0, 2*n*(t-b), (b+t)/(b-t), 0,
+            0, 0, (n+f)/(n-f), -2*f*n/(f-n),
+            0, 0, 1, 0;
+
+    //compute Mcamera
+    //draw lines for all the points
 
     //Link the call backs
     canvas.SetMouseMove(MouseMove);
