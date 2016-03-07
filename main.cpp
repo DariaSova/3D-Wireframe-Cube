4037c24f5299692d44ac2b9184a5c57257b7cb8f
Daria Sova <todariasova@gmail.com>
2016-03-07 12:15:39 -0800

    Clean Up code

--- main.cpp
+++ main.cpp
@@ -21,12 +21,6 @@ Matrix4f perspective_matrix;
 Matrix4f camera_matrix;
 Matrix4f final_matrix;
 
-
-void InitializeGL()
-{
-
-}
-
 void MouseMove(double x, double y)
 {
 
@@ -44,17 +38,14 @@ void KeyPress(char keychar)
 
 void OnPaint()
 {
-   // printf("Matrix Size: %lu\n", points.size());
+  for(int i=0; i<points.size()-1; i++)
+  {
+    Vector4f p = final_matrix*points[i];
+    Vector4f q = final_matrix*points[i+1];
 
-    for(int i=0; i<points.size()-1; i++)///i+2???
-    {
-        Vector4f p = final_matrix*points[i];
-        Vector4f q = final_matrix*points[i+1];
-        //printf("Point X: %f, %f Y: %f, %f\n", p[0], p[1], q[0], q[1]);
+    canvas.AddLine(p[0]/p[2], p[1]/p[2], q[0]/q[2], q[1]/q[2]);
 
-        canvas.AddLine(p[0]/p[2], p[1]/p[2], q[0]/q[2], q[1]/q[2]);
-
-    }
+  }
 }
 
 void OnTimer()
@@ -63,87 +54,76 @@ void OnTimer()
 }
 
 int main(int, char **){
-    /*points.push_back(Vector4f(-1, 1, 1, 1));
-    points.push_back(Vector4f(1, 1, 1, 1));
-    points.push_back(Vector4f(-1, -1, 1, 1));
-    points.push_back(Vector4f(1, -1, 1, 1));
-    points.push_back(Vector4f(-1, 1, -1, 1));
-    points.push_back(Vector4f(1, 1, -1, 1));
-    points.push_back(Vector4f(1, -1, -1, 1));
-    points.push_back(Vector4f(-1, -1, -1, 1));
-    //points.push_back(Vector4f(-1, 1, 1, 1));*/
 
-    //front
-    points.push_back(Vector4f(1, 1, 1, 1));
-    points.push_back(Vector4f(1, -1, 1, 1));
-    points.push_back(Vector4f(-1, -1, 1, 1));
-    points.push_back(Vector4f(-1, 1, 1, 1));
-    points.push_back(Vector4f(1, 1, 1, 1));
+  //front
+  points.push_back(Vector4f(1, 1, 1, 1));
+  points.push_back(Vector4f(1, -1, 1, 1));
+  points.push_back(Vector4f(-1, -1, 1, 1));
+  points.push_back(Vector4f(-1, 1, 1, 1));
+  points.push_back(Vector4f(1, 1, 1, 1));
 
-    //rightside
-    points.push_back(Vector4f(1, 1, -1, 1));
-    points.push_back(Vector4f(1, -1, -1, 1));
-    points.push_back(Vector4f(1, -1, 1, 1));
-    points.push_back(Vector4f(1, 1, 1, 1));
-    points.push_back(Vector4f(1, 1, -1, 1));
+  //rightside
+  points.push_back(Vector4f(1, 1, -1, 1));
+  points.push_back(Vector4f(1, -1, -1, 1));
+  points.push_back(Vector4f(1, -1, 1, 1));
+  points.push_back(Vector4f(1, 1, 1, 1));
+  points.push_back(Vector4f(1, 1, -1, 1));
 
-    //back
-    points.push_back(Vector4f(1, 1, -1, 1));
-    points.push_back(Vector4f(1, -1, -1, 1));
-    points.push_back(Vector4f(-1, -1, -1, 1));
-    points.push_back(Vector4f(-1, 1, -1, 1));
-    points.push_back(Vector4f(1, 1, -1, 1));
+  //back
+  points.push_back(Vector4f(1, 1, -1, 1));
+  points.push_back(Vector4f(1, -1, -1, 1));
+  points.push_back(Vector4f(-1, -1, -1, 1));
+  points.push_back(Vector4f(-1, 1, -1, 1));
+  points.push_back(Vector4f(1, 1, -1, 1));
 
-    //leftside
-    points.push_back(Vector4f(-1, 1, -1, 1));
-    points.push_back(Vector4f(-1, 1, 1, 1));
-    points.push_back(Vector4f(-1, -1, 1, 1));
-    points.push_back(Vector4f(-1, -1, -1, 1));
+  //leftside
+  points.push_back(Vector4f(-1, 1, -1, 1));
+  points.push_back(Vector4f(-1, 1, 1, 1));
+  points.push_back(Vector4f(-1, -1, 1, 1));
+  points.push_back(Vector4f(-1, -1, -1, 1));
 
 
-    //compute Mperspective=Morth*Mper
-    perspective_matrix <<
-            2*n/(r-l), 0, (l+r)/(l-r), 0,
-            0, 2*n/(t-b), (b+t)/(b-t), 0,
-            0, 0, (n+f)/(n-f), 2*f*n/(f-n),
-            0, 0, 1, 0;
+  //compute Mperspective=Morth*Mper
+  perspective_matrix <<
+    2*n/(r-l), 0, (l+r)/(l-r), 0,
+    0, 2*n/(t-b), (b+t)/(b-t), 0,
+    0, 0, (n+f)/(n-f), 2*f*n/(f-n),
+    0, 0, 1, 0;
 
-    //compute Mcamera
-    Vector3f gaze_dir= camera*(-1); //center-eye_pos
-    Vector3f up_vector(0, 1, 0);
-    Vector3f w = -(gaze_dir/gaze_dir.norm());
-    Vector3f u = ((up_vector.cross(w))/(up_vector.cross(w)).norm());
-    Vector3f v = w.cross(u);
+  //compute Mcamera
+  Vector3f gaze_dir= camera*(-1); //center-eye_pos
+  Vector3f up_vector(0, 1, 0);
+  Vector3f w = -(gaze_dir/gaze_dir.norm());
+  Vector3f u = ((up_vector.cross(w))/(up_vector.cross(w)).norm());
+  Vector3f v = w.cross(u);
 
-    Matrix4f MV;
-            MV <<
-               u[0], u[1], u[2], 0,
-               v[0], v[1], v[2], 0,
-               w[0], w[1], w[2], 0,
-               0, 0, 0, 1;
-    printf("MV X: %f, %f Y: %f, %f\n", MV(0,0), MV(0,1), MV(0,2), MV(0,3));
+  Matrix4f MV;
+  MV <<
+    u[0], u[1], u[2], 0,
+    v[0], v[1], v[2], 0,
+    w[0], w[1], w[2], 0,
+    0, 0, 0, 1;
+  printf("MV X: %f, %f Y: %f, %f\n", MV(0,0), MV(0,1), MV(0,2), MV(0,3));
 
-    Matrix4f temp;
-    temp <<
-          1, 0, 0, -camera[0],
-          0, 1, 0, -camera[1],
-          0, 0, 1, -camera[2],
-          0, 0, 0, 1;
+  Matrix4f temp;
+  temp <<
+    1, 0, 0, -camera[0],
+    0, 1, 0, -camera[1],
+    0, 0, 1, -camera[2],
+    0, 0, 0, 1;
 
     camera_matrix = MV*temp;
     final_matrix= perspective_matrix*camera_matrix;
-    //draw lines for all the points
 
     //Link the call backs
     canvas.SetMouseMove(MouseMove);
     canvas.SetMouseButton(MouseButton);
     canvas.SetKeyPress(KeyPress);
+    //Draw cube
     canvas.SetOnPaint(OnPaint);
     canvas.SetTimer(0.05, OnTimer);
     //Show Window
-    canvas.Initialize(width, height, "OpenGL Intro Demo");
-    //Do our initialization
-    InitializeGL();
+    canvas.Initialize(width, height, "3D Wireframe Cube");
     canvas.Show();
 
     return 0;
