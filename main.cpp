29b1a49d53557796878e7a2fa79c692b31ca49c3
Daria Sova <todariasova@gmail.com>
2016-03-07 12:06:51 -0800

    Fix camer position and remove Mvp
    Mviewport trnsformation is taken care by the OpenGl so no need to have
    that matrix

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@ unsigned int width = 512;
 unsigned int height = 512;
 
 Canvas canvas;
-float distance = 10.0f;
-Vector3f camera(1, 0, 0);
-float n = 1.0f;
-float f = 5.0f;
+Vector3f camera(0, 0, 5);
+//visible area on the screen
+float n = -1.0f;
+float f = -10.0f;
 float r = 1.0f;
 float t = 1.0f;
 float b = -1.0f;
 float l = -1.0f;
+
 std::vector<Vector4f> points;
-Matrix4f viewport_matrix;
 Matrix4f perspective_matrix;
 Matrix4f camera_matrix;
 Matrix4f final_matrix;
@@ -50,10 +50,9 @@ void OnPaint()
     {
         Vector4f p = final_matrix*points[i];
         Vector4f q = final_matrix*points[i+1];
-        printf("Point X: %f, %f Y: %f, %f\n", p[0], p[1], q[0], q[1]);
+        //printf("Point X: %f, %f Y: %f, %f\n", p[0], p[1], q[0], q[1]);
 
-        canvas.AddLine(5*p[0]/p[2], 5*p[1]/p[2], 5*q[0]/q[2], 5*q[1]/q[2]);
-        //canvas.AddLine(0,0.2,0.9,0.9);
+        canvas.AddLine(p[0]/p[2], p[1]/p[2], q[0]/q[2], q[1]/q[2]);
 
     }
 }
@@ -102,13 +101,6 @@ int main(int, char **){
     points.push_back(Vector4f(-1, -1, -1, 1));
 
 
-    //compute Mvp
-    viewport_matrix <<
-            width/2, 0, 0, (width-1)/2,
-            0, height/2, 0, (height-1)/2,
-            0, 0, 1, 0,
-            0, 0, 0, 1;
-
     //compute Mperspective=Morth*Mper
     perspective_matrix <<
             2*n/(r-l), 0, (l+r)/(l-r), 0,
@@ -117,8 +109,7 @@ int main(int, char **){
             0, 0, 1, 0;
 
     //compute Mcamera
-    Vector3f eye_position = camera* distance;
-    Vector3f gaze_dir= eye_position*(-1);
+    Vector3f gaze_dir= camera*(-1); //center-eye_pos
     Vector3f up_vector(0, 1, 0);
     Vector3f w = -(gaze_dir/gaze_dir.norm());
     Vector3f u = ((up_vector.cross(w))/(up_vector.cross(w)).norm());
@@ -134,17 +125,13 @@ int main(int, char **){
 
     Matrix4f temp;
     temp <<
-          1, 0, 0, -eye_position[0],
-          0, 1, 0, -eye_position[1],
-          0, 0, 1, -eye_position[2],
+          1, 0, 0, -camera[0],
+          0, 1, 0, -camera[1],
+          0, 0, 1, -camera[2],
           0, 0, 0, 1;
-    printf("TEMP X: %f, %f Y: %f, %f\n", temp(0,0), temp(0,1), temp(0,2), temp(0,3));
 
     camera_matrix = MV*temp;
-
-   //final_matrix= viewport_matrix*perspective_matrix*camera_matrix;
     final_matrix= perspective_matrix*camera_matrix;
-    printf("FINAL X: %f, %f Y: %f, %f\n", final_matrix(0,0), final_matrix(0,1), final_matrix(0,2), final_matrix(0,3));
     //draw lines for all the points
 
     //Link the call backs
